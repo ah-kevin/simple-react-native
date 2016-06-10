efbd4ca3fbad594e3df9f70f2e131a3d4fc9719a
Lennon-pro <kbjwdh@gmail.com>
2016-06-11 00:54:52 +0800

    添加redux-devtool 以及immutable 跟babel

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,6 +4,8 @@ import {autoRehydrate, persistStore} from 'redux-persist';
 import {AsyncStorage} from 'react-native';
 import createLogger from 'redux-logger';
 import reducers from '../reducers'
+import devTools from 'remote-redux-devtools';
+import Immutable from 'immutable';
 
 var isDebuggingInChrome = __DEV__ && !!window.navigator.userAgent;
 var logger = createLogger({
@@ -11,13 +13,24 @@ var logger = createLogger({
   collapsed: true,
   duration: true,
 });
+
+const middlewares = [ thunk, logger ];
+let enhancer;
+if (__DEV__) {
+  const installDevTools = require('immutable-devtools');
+  installDevTools(Immutable);
+  enhancer = compose(
+    applyMiddleware(...middlewares),
+    devTools()
+  );
+} else {
+  enhancer = applyMiddleware(...middlewares);
+}
 export default function configureStore (onComplete:?() => void) {
   const store = createStore(
     reducers,
-    autoRehydrate(),
-    compose(
-      applyMiddleware(thunk, logger)
-    )
+    enhancer,
+    autoRehydrate()
   );
   let opt = {
     storage: AsyncStorage,
